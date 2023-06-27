dbee6ce56b49f767940f00f3972658e96501aeaa
msindreu <msindreuriba@gmail.com>
2023-06-27 15:19:01 +0200

    name window

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:27:46 by msindreu          #+#    #+#             */
-/*   Updated: 2023/06/09 17:50:47 by msindreu         ###   ########.fr       */
+/*   Updated: 2023/06/27 15:17:12 by msindreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 int	main(void)
 {
 	void *mlx;
-	void *mlx_wind;
+	//void *mlx_wind;
 	void *mlx_wind2;
 
 	mlx = mlx_init();
-	mlx_wind = mlx_new_window(mlx, 1920, 1080, "Fractol");
+	//mlx_wind = mlx_new_window(mlx, 1920, 1080, "Fractol");
 	mlx_wind2 = mlx_new_window(mlx, 900, 500, "Fracttititittititiititititit");
 	mlx_loop(mlx);
 }
