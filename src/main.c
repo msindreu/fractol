61ae44bdd4ced47fb3b2b4864f82243c3a60e6b1
msindreu <msindreuriba@gmail.com>
2023-06-28 16:41:19 +0200

    three lines

--- src/main.c
+++ src/main.c
@@ -6,21 +6,51 @@
 /*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:27:46 by msindreu          #+#    #+#             */
-/*   Updated: 2023/06/27 15:17:12 by msindreu         ###   ########.fr       */
+/*   Updated: 2023/06/28 16:38:22 by msindreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mlx/mlx.h"
 #include "../inc/defines.h"
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	              t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(void)
 {
 	void *mlx;
 	//void *mlx_wind;
-	void *mlx_wind2;
+	void *mlx_win;
+	t_data	img;
+	int	i;
 
 	mlx = mlx_init();
+	i = 0;
 	//mlx_wind = mlx_new_window(mlx, 1920, 1080, "Fractol");
-	mlx_wind2 = mlx_new_window(mlx, 900, 500, "Fracttititittititiititititit");
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Fracttititittititiititititit");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	while (i < img.line_length) {
+	my_mlx_pixel_put(&img, i, 1080/4, 0x003355FF);
+	my_mlx_pixel_put(&img, i, 1080/2, 0X00FF1122);
+	my_mlx_pixel_put(&img, i, 1080/2 + 1080/4, 0X0000FF00);
+	i++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+
 }
