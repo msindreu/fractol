45a3537fc9c3f9da96caa0e2dbb35828dd5b3873
msindreu <msindreuriba@gmail.com>
2023-06-30 13:04:04 +0200

    defines colors

--- inc/defines.h
+++ inc/defines.h
@@ -6,8 +6,25 @@
 /*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:41:01 by msindreu          #+#    #+#             */
-/*   Updated: 2023/01/30 17:41:21 by msindreu         ###   ########.fr       */
+/*   Updated: 2023/06/30 13:03:46 by msindreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef DEFINES_H
+# define DEFINES_H
 
+# define X 1920
+# define Y 1080
+
+# define TURQUESA 	0X60FAD2
+# define LILA		0XCD74FC
+# define ROSA		0XFC86C8
+# define VERD		0X8FD19D
+# define AZULCIELO	0X88CCF2
+# define AZULMARIN	0X5760EE
+# define CORAL		0XFA535D
+# define NARANJA	0XFE834D
+# define BLACOROTO	0XF2F2F2
+
+
+#endif
