261553718baa8d86538e6ad43bb1eb94db6ce413
msindreu <msindreuriba@gmail.com>
2023-07-24 10:26:37 +0200

    utils.c

--- src/utils.c
+++ src/utils.c
@@ -6,11 +6,16 @@
 /*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:07:19 by msindreu          #+#    #+#             */
-/*   Updated: 2023/06/30 18:22:15 by msindreu         ###   ########.fr       */
+/*   Updated: 2023/07/24 10:25:14 by msindreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/defines.h"
+
+# define ERROR "Error, please neter a valid argument\n"
+# define PLEASE "Please, write one of the follow options:\n"
+# define JULIA "Julia (x, y)\n"
+# define MANDELBROT "Mandelbrot\n"
 
 void	my_mlx_pixel_put(t_data *data, t_point pixel)
 {
@@ -20,3 +25,22 @@ void	my_mlx_pixel_put(t_data *data, t_point pixel)
 	*(int*)dst = pixel.color;
 }
 
+void	ft_putstr(char *str) 
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0') 
+	{
+		write(2, str + i, 1);
+		i++;
+	}
+}
+
+void	ft_error(void)
+{
+	ft_putstr(ERROR);
+	ft_putstr(PLEASE);
+	ft_putstr(JULIA);
+	ft_putstr(MANDELBROT);
+}
